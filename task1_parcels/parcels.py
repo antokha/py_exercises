c2dfa59791d6ee5bed0fe24a0c051951768ca9ee
Antokha <antokha@gmail.com>
2024-12-01 18:18:10 +0100

    main: exceptions added

--- task1_parcels/parcels.py
+++ task1_parcels/parcels.py
@@ -7,12 +7,28 @@ schedules = {
     "Oceania": [(5, 12.0), (10, 7.0), (15, 5.5), (20, 4.0)],
 }
 
+class ContinentNotFoundError(Exception):
+    pass
+
 def get_price(cont: str, kg: int) -> float:
-    cont_schedule = schedules[cont]
+    cont_schedule = schedules.get(cont, None)
+    if cont_schedule is None:
+        raise ContinentNotFoundError(f"Continent '{cont}' not found")
 
 def main():
-    kg = get_price("France", 23)
-    print(f"kg = {kg}")
+    try:
+        kg = get_price("Africa", 23)
+        print(f"kg = {kg}")
+    except KeyError as exc:
+        print(f"Continent not found: {exc}")
+    except ContinentNotFoundError as exc:
+        print(f"My exception: {exc}")
+        raise Exception("xxx") from exc
+    except Exception as exc:
+        print(f"Unhandled error: {exc}")
+        raise
+    finally:
+        print("Anyway")
 
 if __name__ == '__main__':
     main()
